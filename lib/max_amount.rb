7fddf7fc533d45e9e6c73278e2f5e1b20babe176
Gleb V. Zhegilin <gleboceanborn@gmail.com>
2022-10-01 06:00:04 +0300

    commit 10

--- lib/max_amount.rb
+++ lib/max_amount.rb
@@ -10,11 +10,7 @@ require_relative 'max_amount/errors'
 require_relative 'max_amount/seeker'
 
 # Main MaxAmount module
-module MaxAmount
-  def self.result(custom_string = nil)
-    MaxAmount::CLI.call(custom_string: custom_string)
-  end
-end
+MaxAmount::CLI.call(custom_string: nil)
 
 # путь к файлу с пользовательской строкой
 # path = File.expand_path('./max_amount/data/user_data.txt', __dir__)
